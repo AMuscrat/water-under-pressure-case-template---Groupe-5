81b9b71ac862beae7021bed6c4800a81241b4978
ninasvihrova <nina.svihrova@edu.escp.eu>
2026-09-14 15:30:28 +0200

    Add Southern Europe region indicator dataset

---
+++ south-europe-regions.js
@@ -0,0 +1,11 @@
+window.SOUTH_EUROPE_REGIONS={
+  ESP:[['ES-AN','Andalusia',34,72,-21,48],['ES-MU','Region of Murcia',48,82,-27,72],['ES-VC','Valencia',41,66,-18,55],['ES-CM','Castilla-La Mancha',31,58,-17,43],['ES-AR','Aragon',29,54,-14,39]],
+  PRT:[['PT-AL','Algarve',37,69,-22,26],['PT-AL2','Alentejo',30,61,-19,20],['PT-C','Centro',18,43,-10,12],['PT-N','Norte',10,29,-5,7]],
+  ITA:[['IT-75','Puglia',38,63,-16,42],['IT-88','Calabria',26,55,-14,29],['IT-82','Sicilia',35,70,-21,36],['IT-20','Sardegna',29,52,-12,31],['IT-65','Lazio',21,44,-9,18]],
+  GRC:[['GR-B','Kentriki Makedonia',27,57,-15,27],['GR-F','Dytiki Ellada',24,46,-11,19],['GR-I','Attiki',31,62,-17,12],['GR-K','Peloponnisos',29,59,-16,25],['GR-M','Kriti',36,73,-24,33]],
+  FRA:[['FR-34','Occitanie',22,44,-9,18],['FR-93','Provence-Alpes-Cote d’Azur',28,57,-15,21]],
+  HRV:[['HR-J','Adriatic Croatia',17,42,-9,16],['HR-C','Continental Croatia',13,31,-5,10]],
+  SVN:[['SI-W','Western Slovenia',9,27,-2,7],['SI-E','Eastern Slovenia',12,31,-5,9]],
+  CYP:[['CY-00','Cyprus',53,82,-32,31]],
+  MLT:[['MT-00','Malta',68,73,-25,7]]
+};
