fa77af97af7ef63ab76934969fedc33a40fa1eaa
Vittoria174 <vittoria.farina@edu.escp.eu>
2026-09-15 17:23:50 +0200

    Record final API-backed region list

---
+++ data/dashboard-regions-final-list.js
@@ -0,0 +1,7 @@
+window.AQUACROP_DASHBOARD_REGIONS_FINAL_LIST = {
+  France: ['Ile-de-France','Bourgogne-Franche-Comte','Bretagne','Nouvelle-Aquitaine','Occitanie','Auvergne-Rhone-Alpes','Provence-Alpes-Cote d Azur'],
+  Italy: ['Lombardy','Trentino-Alto Adige','Emilia-Romagna','Tuscany','Lazio','Campania','Puglia','Calabria','Sicilia','Sardegna'],
+  Portugal: ['Norte','Centro','Area Metropolitana de Lisboa','Alentejo','Algarve'],
+  Spain: ['Andalusia','Region of Murcia','Valencia','Castilla-La Mancha','Aragon'],
+  Greece: ['Anatoliki Makedonia & Thraki','Kentriki Makedonia','Ipeiros','Thessalia','Ionia Nisia','Dytiki Ellada','Sterea Ellada','Attiki','Peloponnisos','Voreio Aigaio','Notio Aigaio','Kriti']
+};
